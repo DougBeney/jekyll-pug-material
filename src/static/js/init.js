60de92a2d63aa575647265d5221584a98e7def18
Doug Beney <work@dougie.io>
2017-11-12 22:24:33 -0500

    Added sourcecode thing. Tweas

--- src/static/js/init.js
+++ src/static/js/init.js
@@ -2,6 +2,10 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    if (!sessionStorage.alreadyClicked) {
+    	$('.tap-target').tapTarget('open');
+        sessionStorage.alreadyClicked = 1;
+    }
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
